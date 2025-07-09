19dc402482c75a8f4e67815a8b40465d183951b8
yagami691 <pensnko@gmail.com>
2025-07-09 15:00:24 +0100

    chore: add updates in UserMapper and UserController

--- src/main/java/com/User/Service/UserBookList.java
+++ src/main/java/com/User/Service/UserBookList.java
@@ -2,6 +2,7 @@ package com.User.Service;
 
 import com.User.Service.dto.BookResponseDto;
 import com.User.Service.entities.User;
+import lombok.RequiredArgsConstructor;
 import org.mapstruct.Context;
 import org.mapstruct.Named;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,10 +12,11 @@ import org.springframework.stereotype.Component;
 import java.util.List;
 
 @Component
+@RequiredArgsConstructor
 public class UserBookList {
 
-    @Autowired
-    private BookClient bookClient;
+
+    private final BookClient bookClient;
 
     @Named("resolveBookListWithPageable")
     public List<BookResponseDto> resolve(User user, @Context Pageable pageable) {
